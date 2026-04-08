4d136f91f56535028ef146b7124a135e344a2b7b
Lan Lan <llanlan91@gmail.com>
2026-04-08 12:24:36 +0800

    fix parsing issues

--- src/components/dialog-sections/RightSideFields.vue
+++ src/components/dialog-sections/RightSideFields.vue
@@ -111,12 +111,12 @@ watch(form, () => {
   emit('update:modelValue', { ...form })
 }, { deep: true })
 
-watch(() => props.modelValue, (newVal) => {
+watch(() => ({ ...props.modelValue }), (newVal) => {
   if (!newVal) return
   for (const [key, value] of Object.entries(newVal)) {
     if (key in form) form[key] = value
   }
-}, { deep: true })
+})
 
 function handleSubmit() {
   emit('submit')
